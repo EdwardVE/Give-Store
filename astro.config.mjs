7ee1ba43bd4e258e6ed1fa2d89fb851baba97792
EdwardVE <ervasalloe@unal.edu.co>
2025-10-16 14:05:47 -0500

    home

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://give-store.vercel.app/",
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
