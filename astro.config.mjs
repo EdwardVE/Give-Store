06f4c95e50ac706f1f7fabcefde783469cac7295
EdwardVE <ervasalloe@unal.edu.co>
2025-10-17 11:46:18 -0500

    filtro etapa 1, en esta etapa estoy creando el path con una sola categoria

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  // output: "server",//!TODO: para produccion y que la construccion se haga en el servidor, sobre todo en lo de filtros
   site: "https://give-store.vercel.app/",
   integrations: [sitemap(), icon()],
   vite: {
